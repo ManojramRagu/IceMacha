60fdd31b4a4ffc805f88dde9823beaa94ebe3f05
ManojramRagu <manojramragu@gmail.com>
2026-01-24 23:23:55 +0530

    Changed cart to icon

--- resources/views/livewire/cart-icon.blade.php
+++ resources/views/livewire/cart-icon.blade.php
@@ -1,11 +1,8 @@
 <div class="relative inline-flex align-middle">
-    <a href="#" class="text-gray-500 hover:text-brand transition-colors duration-150">
-        <img src="{{ asset('img/icons/cart.svg') }}" alt="Cart" class="w-6 h-6">
-        
-        @if($cartCount > 0)
-            <span class="absolute -top-2 -right-2 inline-flex items-center justify-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-bold leading-none text-white bg-brand shadow-sm">
-                {{ $cartCount }}
-            </span>
-        @endif
+    <a href="#" class="text-white hover:text-sand transition-colors duration-150">
+        <img src="{{ asset('img/icons/cart.svg') }}" alt="Cart" class="w-6 h-6 brightness-0 invert">
+        <span class="absolute -top-2 -right-2 inline-flex items-center justify-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-bold leading-none text-white bg-brand shadow-sm">
+            {{ $cartCount }}
+        </span>
     </a>
 </div>
